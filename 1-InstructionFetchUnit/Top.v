fd2ffc54091bd88da0f87b4adbb116400b37581c
Kamil Frem <137979579+KamilFrem@users.noreply.github.com>
2026-08-31 19:09:29 -0700

    Refactor Top module to include inputs and outputs

--- 1-InstructionFetchUnit/Top.v
+++ 1-InstructionFetchUnit/Top.v
@@ -1,19 +1,21 @@
 `timescale 1ns / 1ps
 
 
-module Top();
-    reg Clk;
-    reg Reset;
+module Top(Clk, Reset, out7, en_out);
+    input Clk;
+    output [6:0] out7;
+    output [7:0] en_out;
+    input Reset;
     wire [15:0] instruct;
     wire [15:0] Result;
     wire [31:0] Instruction;
     wire [31:0] PCResult;
     wire ClkOut;
-    ClkDiv(Clk, 0, ClkOut);
-    InstructionFetchUnit(Instruction, PCResult, Reset, ClkOut);
+    ClkDiv a1(Clk, 1'b0, ClkOut);
+    InstructionFetchUnit u2(Instruction, PCResult, Reset, ClkOut);
     assign instruct = Instruction[15:0];
     assign Result = PCResult[15:0];
-    Two4DigitDisplay(Clk, instruct, Result, out7, en_out);
+    Two4DigitDisplay u3(Clk, instruct, Result, out7, en_out);
    
    
 endmodule
