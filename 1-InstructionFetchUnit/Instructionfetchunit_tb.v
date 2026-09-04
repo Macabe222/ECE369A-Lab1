134ae2411295189e8a64024dd714c68a71ccfbe1
Kamil Frem <137979579+KamilFrem@users.noreply.github.com>
2026-09-03 19:59:23 -0700

    Clean up comments in Instructionfetchunit_tb.v
    Removed header comments from Instructionfetchunit_tb.v

--- 1-InstructionFetchUnit/Instructionfetchunit_tb.v
+++ 1-InstructionFetchUnit/Instructionfetchunit_tb.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 08/31/2026 06:55:24 PM
-// Design Name: 
-// Module Name: Instructionfetchunit_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module Instructionfetchunit_tb();
     
