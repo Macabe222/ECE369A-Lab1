22d2407dd71a18bc432bc20266f63d8f003f1bb2
Macabe Wood <macabewood@gmail.com>
2026-09-23 14:14:39 -0700

    delete some files

--- ALU32Bit/ALU32Bit.v
+++ ALU32Bit/ALU32Bit.v
@@ -2,7 +2,7 @@
 
 ////////////////////////////////////////////////////////////////////////////////
 // ECE369 - Computer Architecture
-// 
+//
 // Module - ALU32Bit.v
 // Description - 32-Bit wide arithmetic logic unit (ALU).
 //
@@ -13,17 +13,17 @@
 //
 // OUTPUTS:-
 // ALUResult: 32-Bit ALU result output.
-// ZERO: 1-Bit output flag. 
+// ZERO: 1-Bit output flag.
 //
 // FUNCTIONALITY:-
-// Design a 32-Bit ALU, so that it supports all arithmetic operations 
-// needed by the MIPS instructions given in Labs5-8.docx document. 
-//   The 'ALUResult' will output the corresponding result of the operation 
-//   based on the 32-Bit inputs, 'A', and 'B'. 
-//   The 'Zero' flag is high when 'ALUResult' is '0'. 
-//   The 'ALUControl' signal should determine the function of the ALU 
-//   You need to determine the bitwidth of the ALUControl signal based on the number of 
-//   operations needed to support. 
+// Design a 32-Bit ALU, so that it supports all arithmetic operations
+// needed by the MIPS instructions given in Labs5-8.docx document.
+//   The 'ALUResult' will output the corresponding result of the operation
+//   based on the 32-Bit inputs, 'A', and 'B'.
+//   The 'Zero' flag is high when 'ALUResult' is '0'.
+//   The 'ALUControl' signal should determine the function of the ALU
+//   You need to determine the bitwidth of the ALUControl signal based on the number of
+//   operations needed to support.
 ////////////////////////////////////////////////////////////////////////////////
 
 module ALU32Bit(ALUControl, A, B, ALUResult, Zero);
@@ -35,7 +35,9 @@ module ALU32Bit(ALUControl, A, B, ALUResult, Zero);
 	output [31:0] ALUResult;	// answer
 	output Zero;	    // Zero=1 if ALUResult == 0
 
+    always @ begin
+
+    end
     /* Please fill in the implementation here... */
 
 endmodule
-
--- test.py
+++
@@ -1,6 +0,0 @@
-
-
-row = 0
-col = 0
-
-for row in range(4):
